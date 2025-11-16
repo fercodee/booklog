340c42d8d17ed00cf0b8a2c25d8d56635f9b27f7
fercodee <103159404+fercodee@users.noreply.github.com>
2025-11-16 03:56:43 -0300

    chore: dotenv config

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,15 @@ import 'package:book_log/ui/core/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  await Supabase.initialize(
-    url: 'YOUR_SUPABASE_URL',
-    anonKey: 'YOUR_SUPABASE_ANON_KEY',
-  );
+  await dotenv.load(fileName: '.env');
+  final url = dotenv.env['SUPABASE_URL']!;
+  final key = dotenv.env['SUPABASE_ANON_KEY']!;
+  await Supabase.initialize(url: url, anonKey: key);
   
   runApp(const MyApp());
 }
